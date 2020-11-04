84529e916c19a33cbe484a038bdf6ec76731dca5
Being Scholar <beingscholar@gmail.com>
2020-11-04 14:38:01 +0530

    Remove extra category title

--- wp-content/plugins/woocommerce/includes/wc-template-functions.php
+++ wp-content/plugins/woocommerce/includes/wc-template-functions.php
@@ -966,11 +966,11 @@ if ( ! function_exists( 'woocommerce_content' ) ) {
 		} else {
 			?>
 
-			<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+			<?php /* if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
 				<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
 
-			<?php endif; ?>
+			<?php endif; */ ?>
 
 			<?php do_action( 'woocommerce_archive_description' ); ?>
 
