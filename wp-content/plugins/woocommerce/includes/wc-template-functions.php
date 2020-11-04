2255752258cd9463f03e438d397efcae9529ce47
Being Scholar <beingscholar@gmail.com>
2020-11-04 14:27:39 +0530

    About Us Heading Title

--- wp-content/plugins/woocommerce/includes/wc-template-functions.php
+++ wp-content/plugins/woocommerce/includes/wc-template-functions.php
@@ -1230,10 +1230,9 @@ if ( ! function_exists( 'woocommerce_taxonomy_archive_description' ) ) {
 	function woocommerce_taxonomy_archive_description() {
 		if ( is_product_taxonomy() && 0 === absint( get_query_var( 'paged' ) ) ) {
 			$term = get_queried_object();
-
 			if ( $term && ! empty( $term->description ) ) {
 				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-				echo '<div class="about-box"><div class="about-box--head"><p>About ' . $term->title . '</p></div><div class="about-box--body">' . wc_format_content( $term->description ) . '</div></div>';
+				echo '<div class="about-box"><div class="about-box--head"><p>About ' . $term->name . '</p></div><div class="about-box--body">' . wc_format_content( $term->description ) . '</div></div>';
 			}
 		}
 	}
