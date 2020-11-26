54d8c6058e0002fef3cf359a64d5ad57f0c38b34
Being Scholar <beingscholar@gmail.com>
2020-11-26 16:10:55 +0530

    footer fixes

--- wp-content/themes/electro/functions.php
+++ wp-content/themes/electro/functions.php
@@ -196,7 +196,7 @@ function vna_is_purchasable( $purchasable, $product ){
     return true || false; // depending on your condition
 }
 
-add_action('template_redirect','check_if_logged_in');
+/* add_action('template_redirect','check_if_logged_in');
 function check_if_logged_in() {
     $pageid = get_option( 'woocommerce_checkout_page_id' );
     if(!is_user_logged_in() && is_page($pageid)) {
@@ -218,4 +218,4 @@ function check_if_logged_in() {
     
         }
     }
-}
+} */
