df62f16e83071e1d913068ffd14c8390bedce71f
A1M8QE66 <a1m8qe66@LTA1M8QE66-MAC.local>
2020-12-09 16:10:46 +0530

    UI Fix

--- wp-content/plugins/woocommerce/templates/checkout/thankyou.php
+++ wp-content/plugins/woocommerce/templates/checkout/thankyou.php
@@ -38,7 +38,6 @@ defined( 'ABSPATH' ) || exit;
 			</p>
 
 		<?php else : ?>
-
 			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
